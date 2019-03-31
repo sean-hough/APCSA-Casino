ba569adc8da2e2b5ab77e813ea5cdc9f163f6272
sean-hough <shough266@gmail.com>
2019-03-31 18:27:41 -0400

    added credit where credit is due

--- src/Card.java
+++ src/Card.java
@@ -4,7 +4,7 @@ public class Card {
 	private String rank;
 	private int value;
 
-
+	//constructor inspired by AP Lab elevens
 	public Card(String suit, String rank, int value) {
 		this.rank = rank;
 		this.suit = suit;
@@ -14,13 +14,14 @@ public class Card {
 	public int getValue() {
 		return value;
 	}
-	
 	public String getSuit() {
 		return suit;
 	}
 	public String getRank() {
 		return rank;
 	}
+	
+	//toString inspired by AP Lab elevens
 	public String toString() {
 		return rank + " of " + suit;
 	}
--- src/Deck.java
+++ src/Deck.java
@@ -2,13 +2,10 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
-/**
- * The Deck class represents a shuffled deck of cards.
- * It provides several operations including
- *      initialize, shuffle, deal, and check if empty.
- */
+
 public class Deck extends ArrayList<Card>{
 
+	//constructor inspired by AP Lab elevens
 	public Deck(String[] suits, String[] ranks, int[] values) {
 		for (int i = 0; i < ranks.length; i++) {
 			for (String suitString : suits) {
